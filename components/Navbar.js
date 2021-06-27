9eec0532fa66d89b4ea890ba18ff02a4b33453a9
Erin Lemkey <elemkey@gmail.com>
2021-06-27 09:19:51 -0400

    trying map file as tsx

---
+++ components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Colors from 'react-native';
+import {ProfileTabNavigator} from 'react-native-navbar'
+import Comments from 'Comments';
+
+const NavBar = () => {
+  return (
+      <ProfileTabNavigator.Navigator tabBarOptions={{
+          activeTintColor: Colors.COLOR_WHITE,
+          labelStyle: {
+              textTransform: "uppercase",
+          },
+          inactiveTintColor: Colors.COLOR_SUPER_DARK_GREY,
+          indicatorStyle: {
+              height: null,
+              top: '10%',
+              bottom: '10%',
+              width: '45%',
+              left: '2.5%',
+              borderRadius: 100,
+              backgroundColor: Colors.PRIMARY_TWO,
+          },
+          style: {
+              alignSelf: "center",
+              width: '50%',
+              borderRadius: 100,
+              borderColor: "blue",
+              backgroundColor: "white",
+              elevation: 5, 
+              shadowOpacity: .10,
+              shadowRadius: 4, 
+          },
+          tabStyle: {
+              borderRadius: 100,
+          },
+      }}
+      swipeEnabled={true}>
+        <h2>Kick A Building</h2>
+   <ProfileTabNavigator.Screen name="Comments" component={Comments} />
+ </ProfileTabNavigator.Navigator>
+);
+};
+
+export default NavBar;
